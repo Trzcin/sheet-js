23b1c4c3b22bbf48bca915e43880821604069795
Mikołaj Trzciński <trzccin@gmail.com>
2024-04-17 20:34:46 +0200

    feat: add width and height prop

--- src/components/Spreadsheet.tsx
+++ src/components/Spreadsheet.tsx
@@ -1,32 +1,51 @@
-export default function Spreadsheet() {
+import { useMemo } from "react";
+
+interface SpreadsheetProps {
+    width: number;
+    height: number;
+}
+
+export default function Spreadsheet(props: SpreadsheetProps) {
+    const columnNames = useMemo(() => (
+        Array.from({length: props.width}, (_, i) => columnName(i))
+    ), [props.width]);
+
     return <table>
         <thead>
             <tr>
                 <th></th>
-                <th>A</th>
-                <th>B</th>
-                <th>C</th>
+                {columnNames.map((name, i) => (
+                    <th key={i}>{name}</th>
+                ))}
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>1</td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td></td>
-                <td>5</td>
-                <td></td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td></td>
-                <td>2.1</td>
-                <td>3.4</td>
-            </tr>
+            {Array.from({ length: props.height }, (_, row) => (
+                <tr key={row}>
+                    <td>{row+1}</td>
+                    {Array.from({ length: props.width }, (_, col) => (
+                        <td key={col}></td>
+                    ))}
+                </tr>
+            ))}
         </tbody>
     </table>;
+}
+
+function columnName(index: number): string {
+    let name = "";
+    let currNum = index + 1;
+
+    while (currNum > 0) {
+        const remainder = currNum % 26;
+        if (remainder == 0) {
+            name = 'Z' + name;
+            currNum = Math.floor(currNum / 26) - 1;
+        } else {
+            name = String.fromCharCode(65 - 1 + remainder) + name;
+            currNum = Math.floor(currNum / 26);
+        }
+    }
+    
+    return name;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,6 @@ import Spreadsheet from './components/Spreadsheet';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Spreadsheet />
+        <Spreadsheet width={16} height={20}/>
     </React.StrictMode>,
 );
