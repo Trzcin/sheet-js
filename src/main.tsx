d1a253d9298f326ad7df4df139051fed1399e404
Mikołaj Trzciński <trzccin@gmail.com>
2024-04-26 18:47:06 +0200

    fix: update the initial cell values to make testing graphs easier

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,14 @@ import Spreadsheet from './components/Spreadsheet';
 import CellMap from './CellMap';
 
 const data = new CellMap();
-data.set({ x: 1, y: 1 }, 0.3);
-data.set({ x: 1, y: 2 }, 5);
-data.set({ x: 1, y: 3 }, 2.7);
+data.set({ x: 1, y: 1 }, 1);
+data.set({ x: 1, y: 2 }, 2);
+data.set({ x: 1, y: 3 }, 3);
+data.set({ x: 1, y: 4 }, 4);
+data.set({ x: 2, y: 1 }, 2);
+data.set({ x: 2, y: 2 }, 4);
+data.set({ x: 2, y: 3 }, 8);
+data.set({ x: 2, y: 4 }, 16);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
